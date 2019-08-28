35cd8c6fc7535c060d211ec4dd6ce6cce743e4c4
jpsthecelt <jpsthecelt@gmail.com>
2019-08-28 13:12:34 -0700

    modified neo6mExample & made various mods to basic and gps

--- basic.py
+++ basic.py
@@ -98,4 +98,5 @@ if  __name__ == '__main__':
         print("\nKilling Thread...")
         gpsp.running = False
         gpsp.join()  # wait for the thread to finish what it's doing
+
     print("Done.\nExiting.")
---
+++ neo6mExample.py
@@ -0,0 +1,19 @@
+import serial
+import pynmea2
+
+def parseGPS(str):
+    if str.find('GGA') > 0:
+       msg = pynmea2.parse(str)
+       print("Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" %
+            (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))
+
+if __name__ == '__main__':
+    try:
+        with serial.Serial('/dev/serial0', 9600, timeout=1.0) as sp:
+           while True:
+               byteChars = sp.readline()
+               parseGPS(byteChars.decode('utf-8'))
+
+    except KeyboardInterrupt:
+        print('^C pressed -- exiting script')
+
